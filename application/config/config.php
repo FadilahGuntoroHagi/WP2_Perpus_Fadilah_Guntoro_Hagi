440f5fb80e81e0b7cfa0006e789c06386bef9c11
FadilahGuntoroHagi <93064623+FadilahGuntoroHagi@users.noreply.github.com>
2021-12-06 11:40:20 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/WP2_Perpus_FadiyaNurFadhilah/';
+$config['base_url'] = 'http://localhost/Pustaka_Booking/';
 
 /*
 |--------------------------------------------------------------------------
